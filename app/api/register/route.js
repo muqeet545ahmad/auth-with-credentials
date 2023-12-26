95b1fc9c63afce527150a51b02e78f5e4cad4102
muqeet545ahmad <inoxentuni1@gmail.com>
2023-12-27 00:50:50 +0500

    aaa

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -6,10 +6,16 @@ import bcrypt from "bcryptjs";
 export async function POST(req) {
   try {
     const { name, email, password } = await req.json();
+
+    console.log("Name=", name);
+    console.log("Email=", email);
+    console.log("password=", password);
+
     const hashedPassword = await bcrypt.hash(password, 10);
     await connectMongoDB();
-    await User.create({ name, email, password: hashedPassword });
-
+    // await User.create({ name, email, password: hashedPassword });
+    const newUser = await User.create({ name, email, password: hashedPassword });
+    console.log("New User=", newUser);
     return NextResponse.json({ message: "User registered." }, { status: 201 });
   } catch (error) {
     return NextResponse.json(
